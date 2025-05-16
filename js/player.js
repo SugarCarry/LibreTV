889299cc1340a22f3ca6ea4db0b5977c1684b91a
SugarCarry <115970985+SugarCarry@users.noreply.github.com>
2025-05-16 20:23:48 +0800

    Update player.js

--- js/player.js
+++ js/player.js
@@ -397,17 +397,17 @@ function initPlayer(videoUrl, sourceCode) {
         airplay: true,                   // 在Safari中启用AirPlay功能
         chromecast: true,                // 启用Chromecast投屏功能
         contextmenu: [                   // 自定义右键菜单
-
-
-
-
-
-
-
-
-
-
-            
+            {
+                text: '关于 BestTV',
+                link: 'https://github.com/LibreSpark/LibreTV'
+            },
+            {
+                text: '问题反馈',
+                click: (player) => {
+                    window.open('https://github.com/LibreSpark/LibreTV/issues', '_blank');
+                }
+            }
+        ],  
         video: {
             url: videoUrl,
             type: 'hls',
